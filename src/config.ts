790335f84e6eb6d3f7892660fc7d905d55c7c136
Anuranjan Srivastava <anuranjan.srivastava@decentro.tech>
2024-09-10 19:58:23 +0530

    Added decentrotech-fabric-react-sdk

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+// Client Credentials
+export const CLIENT_ID = ""
+export const CLIENT_SECRET = "";
+
+// Basic UIStream config
+export const UISTREAM_SESSION_API_URL = "https://in.staging.decentro.tech/v2/kyc/workflows/uistream";
+export const UISTREAM_REDIRECT_URL = "https://google.com";
+export const UISTREAM_CALLBACK_URL = "https://webhook.site/dbe241ca-2533-49b4-9220-d479e1208f98";
+export const UISTREAM_FLOW_NAME = "DIGILOCKER_AADHAAR_PAN"
